810b6fa1bfb624786e089776c3c261c08223449e
oliverlai55 <oliverlai55@gmail.com>
2016-11-09 02:48:12 -0500

    Make changes to render data

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
+class PostsIndex extends Component {
+
+
+    render() {
+      return (
+        <div>
+            Test
+        </div>
+      )
+    }
+}
