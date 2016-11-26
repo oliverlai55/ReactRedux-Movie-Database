780ec71af1348f13aefbc590b317845d0dae2dd7
oliverlai55 <oliverlai55@gmail.com>
2016-11-26 09:47:10 -0500

    Set up reducer for fetch movie

--- src/reducers/reducer_movies.js
+++ src/reducers/reducer_movies.js
@@ -1,9 +1,11 @@
-import { FETCH_MOVIES } from '../actions/index';
+import { FETCH_MOVIES, FETCH_MOVIE } from '../actions/index';
 
 const INITIAL_STATE = { all: [] };
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
+  case FETCH_MOVIE:
+    return { ...state, all: action.payload.data };
   case FETCH_MOVIES:
     return { ...state, all: action.payload.data };
   default:
