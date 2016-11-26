e610088e9e367c16da81e83798b8351d7921e879
oliverlai55 <oliverlai55@gmail.com>
2016-11-26 09:55:21 -0500

    Complete movie reducer

--- src/reducers/reducer_movies.js
+++ src/reducers/reducer_movies.js
@@ -1,11 +1,11 @@
 import { FETCH_MOVIES, FETCH_MOVIE } from '../actions/index';
 
-const INITIAL_STATE = { all: [] };
+const INITIAL_STATE = { all: [], movie: null };
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
   case FETCH_MOVIE:
-    return { ...state, all: action.payload.data };
+    return { ...state, movie: action.payload.data };
   case FETCH_MOVIES:
     return { ...state, all: action.payload.data };
   default:
