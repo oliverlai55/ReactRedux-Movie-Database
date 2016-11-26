9a614c6c33f349115bcf4b21b7c2cc25750bcb05
oliverlai55 <oliverlai55@gmail.com>
2016-11-26 11:22:48 -0500

    Complete renderMovie function

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -12,8 +12,29 @@ class PostsShow extends Component {
     this.props.fetchMovie(this.props.params.id);
   }
 
+
+  renderMovie() {
+    const { movie } = this.props;
+    if (movie) {
+      let movieImageUrl = `http://image.tmdb.org/t/p/w300${movie.poster_path}`;
+      console.log(movieImageUrl);
+      console.log(movie.poster_path);
+
+      return (
+          <div className="card col-lg-3 col-md-4 col-sm-6 col-xs-12">
+
+                  <img className="card-img-top" src={movieImageUrl} alt="Image N/A" />
+
+          </div>
+      )
+    }
+  }
+
+
   render() {
     const { movie } = this.props;
+    console.log("====MOVIE DETAIL====");
+    console.log(movie);
 
     if (!movie) {
       return (
@@ -24,6 +45,7 @@ class PostsShow extends Component {
     return (
       <div>
         {movie.title}
+        {this.renderMovie()}
       </div>
     )
   }
