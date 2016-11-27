5e3da31ad64e178af28d5c8f3184a5d9aac8cc8c
oliverlai55 <oliverlai55@gmail.com>
2016-11-27 16:51:18 -0500

    Create filterbar component

---
+++ src/components/filterbar.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchCollection } from '../actions/index';
+import { Link } from 'react-router';
