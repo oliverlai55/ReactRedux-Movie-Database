2ba72b988599baffb1d66889ddf4b583559184c0
oliverlai55 <oliverlai55@gmail.com>
2016-11-29 00:03:17 -0500

    Create sortMovie action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 export const FETCH_MOVIES = 'FETCH_MOVIES';
 export const FETCH_MOVIE = 'FETCH_MOVIE';
+export const SORT_MOVIES = 'SORT_MOVIES';
 
 const ROOT_URL = 'http://api.themoviedb.org/3/movie/';
 const API_KEY = 'f40bb4460c5fd3208bf382531a59218a';
 
 export function fetchCollection() {
   const request = axios.get(`${ROOT_URL}now_playing?api_key=${API_KEY}`)
-
+  console.log(request);
   return {
     type: FETCH_MOVIES,
     payload: request
@@ -21,5 +22,13 @@ export function fetchMovie(id) {
   return {
     type: FETCH_MOVIE,
     payload: request
+  };
+}
+
+export function sortMovies(sortKey) {
+    console.log('inside action creator');
+  return {
+    type: SORT_MOVIES,
+    payload: sortKey
   };
 }
