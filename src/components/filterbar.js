16ee94b8c6274cab6d58623ad347eea5875e4a33
oliverlai55 <oliverlai55@gmail.com>
2016-11-29 21:20:39 -0500

    Increase readability

--- src/components/filterbar.js
+++ src/components/filterbar.js
@@ -9,67 +9,24 @@ class FilterBar extends Component {
   //may NOT need props since we still have access to it later
   constructor(props) {
     super(props)
-    // console.log("This is props: " + this.props.filterMovies);
     this.state = {
       selectedVideo: 'Playing'
     };
 
-    // this.logChange = this.logChange.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
   }
 
-  // logChange(event) {
-  //   // console.log('logchange');
-  // }
 
   onInputChange() {
-    console.log("INPUT CHANGE SELECT");
+    console.log("INPUT CHANGE Function");
     var value = React.findDOMNode(this.refs.mySelect).value;
     this.props.sortMovies(value);
 
   }
 
   render() {
-    // console.log(this.props.filterMovies('hola filter'));
-    // console.log("STATE: " + this.state.selectedVideo);
-    // let selectedVideo = this.state.selectedVideo;
-    // var options = [
-    //   { value: 'one', label: 'One'},
-    //   { value: 'two', label: "Two"}
-    // ];
-    //
-    // function logChange(val) {
-    //   console.log(val.target.value);
-    // }
 
     return (
-      // <div className="filter-bar col-sm-12">
-      //   <div className="dropdown open">
-      //     <a className="btn btn-sm btn-secondary dropdown-toggle" href="https://example.com" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      //       Dropdown link
-      //     </a>
-      //
-      //     <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-      //       <a className="dropdown-item" href="#">Action</a>
-      //       <a className="dropdown-item" href="#">Another action</a>
-      //       <a className="dropdown-item" href="#">Something else here</a>
-      //     </div>
-      //   </div>
-      //
-      // </div>
-
-
-      // <Select
-      //     className="filter-bar"
-      //     name="form-field-name"
-      //     value={selectedVideo}
-      //     options={options}
-      //     onChange={logChange}
-      // />
-
-
-
-      // Change state
       <div>
         <form onSubmit={this.logChange}>
           <div className="form-group">
