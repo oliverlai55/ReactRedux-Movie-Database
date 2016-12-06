427b4b557fe6457357fcd01e74ad2986e04ae089
oliverlai55 <oliverlai55@gmail.com>
2016-12-05 23:50:06 -0500

    Implement routing for About page

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsShow from './components/posts_show';
+import About from './components/about';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="movie/:id" component={PostsShow} />
+    <Route path="/about" component={About} />
   </Route>
 );
