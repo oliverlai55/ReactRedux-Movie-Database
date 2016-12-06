454f8e707f4f7e51535b1af2034c9c494110ab36
oliverlai55 <oliverlai55@gmail.com>
2016-12-05 20:16:04 -0500

    Clean navbar layout

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -36,29 +36,18 @@ class NavBar extends Component {
       <div>
         <nav className="navbar navbar-fixed-top navbar-full navbar-dark bg-inverse">
           <Link to="/">
-            <a className="navbar-brand" onClick={this.reloadPage} href="#">MovieMaster</a>
+            <a className="navbar-brand" onClick={this.reloadPage} href="#"><img className="logo-image" src="../src/img/moviemaster.png" /></a>
           </Link>
           <ul className="nav navbar-nav pull-sm-right">
             <li className="nav-item">
               <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div className="dropdown-menu" aria-labelledby="supportedContentDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
             </li>
           </ul>
           <form onSubmit={this.onFormSubmit} className="navsearch-bar form-inline pull-sm-right">
             <input
               className="form-control"
               type="text"
-              placeholder="Search"
+              placeholder="Search Movie Title"
               value={this.state.term}
               onChange={this.onInputChange} />
               <button className="btn btn-outline-success" type="submit">Search</button>
